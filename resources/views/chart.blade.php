6a4367dcf573e74d851220877c46de69a0e7d0f1
Chanom17 <640112418093@bru.ac.th>
2023-10-11 11:25:20 +0700

    no profile border

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -109,8 +109,8 @@
                     datasets: [{
                         label: 'Votes',
                         data: votes,
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
                         borderWidth: 1
                     }]
                 },
